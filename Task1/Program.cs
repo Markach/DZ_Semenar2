40b35accedec6d22d0731c710f7aff65a3711cd1
ala markach <drozdowa1958@gmail.com>
2022-08-24 16:43:02 +0400

    add Task001

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+static void PrintNumbers ()
+{
+
+Console.WriteLine("Введите целое трехзначное число: ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+
+    if((numbers >= 100 && numbers < 1000))
+    {
+        Console.WriteLine(numbers + " трехзначное число");
+        int resalt = (numbers / 10) % 10;
+        Console.WriteLine(resalt + " вторая цифра введенного числа");
+    }
+     else
+    {
+        Console.WriteLine("Вы ввели не трехзначное число ");
+    }
+}
+PrintNumbers ();
+
